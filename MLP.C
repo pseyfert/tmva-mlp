pseyfert/tmva-mlp

commit a8c98d35c23d18ccd803b3306cb4e023c2fd4020

parallelize more aggressively

MLP.C
@@ -823,41 +823,44 @@ inline float ReadMLP::GetMvaValue__( const std::vector<float>& inputValues )
   float retval(0.f);
 
   // layer 0 to 1
-  for (int o=0; o<27-1; o++) {
-    __m128 simd_in = _mm_load_ps(&inputValues[0]);
-    __m128 matrix = _mm_load_ps(&fWeightMatrix0to1[o][0]);
-    __m128 sum = _mm_mul_ps(simd_in,matrix);
-    simd_in = _mm_load_ps(&inputValues[4]);
-    matrix = _mm_load_ps(&fWeightMatrix0to1[o][4]);
-    __m128 c =  _mm_mul_ps(simd_in,matrix);
-    sum = _mm_add_ps(c,sum);
+  for (int o=0; o<27-1; o+=4) {
+    __m128 sum[4];
+    for (int oo = 0 ; (oo<4&&o+oo<27-1) ; oo++) {
+      __m128 simd_in = _mm_load_ps(&inputValues[0]);
+      __m128 matrix = _mm_load_ps(&fWeightMatrix0to1[o][0]);
+      sum[oo] = _mm_mul_ps(simd_in,matrix);
+      simd_in = _mm_load_ps(&inputValues[4]);
+      matrix = _mm_load_ps(&fWeightMatrix0to1[o][4]);
+      __m128 c =  _mm_mul_ps(simd_in,matrix);
+      sum[oo] = _mm_add_ps(c,sum[oo]);
 
-    simd_in = _mm_load_ps(&inputValues[8]);
-    matrix = _mm_load_ps(&fWeightMatrix0to1[o][8]);
-    c =  _mm_mul_ps(simd_in,matrix);
-    sum = _mm_add_ps(c,sum);
-
-
-    simd_in = _mm_load_ps(&inputValues[12]);
-    matrix = _mm_load_ps(&fWeightMatrix0to1[o][12]);
-    c =  _mm_mul_ps(simd_in,matrix);
-    sum = _mm_add_ps(c,sum);
+      simd_in = _mm_load_ps(&inputValues[8]);
+      matrix = _mm_load_ps(&fWeightMatrix0to1[o][8]);
+      c =  _mm_mul_ps(simd_in,matrix);
+      sum[oo] = _mm_add_ps(c,sum[oo]);
 
 
-    simd_in = _mm_load_ps(&inputValues[16]);
-    matrix = _mm_load_ps(&fWeightMatrix0to1[o][16]);
-    c =  _mm_mul_ps(simd_in,matrix);
-    sum = _mm_add_ps(c,sum);
+      simd_in = _mm_load_ps(&inputValues[12]);
+      matrix = _mm_load_ps(&fWeightMatrix0to1[o][12]);
+      c =  _mm_mul_ps(simd_in,matrix);
+      sum[oo] = _mm_add_ps(c,sum[oo]);
 
-    simd_in = _mm_load_ps(&inputValues[20]);
-    matrix = (_mm_setr_ps(fWeightMatrix0to1[o][20],fWeightMatrix0to1[o][21],0.f,0.f));
-    c =  _mm_mul_ps(simd_in,matrix);
-    sum = _mm_add_ps(c,sum);
 
-    sum = _mm_hadd_ps(sum,sum);
-    sum = _mm_hadd_ps(sum,sum);
+      simd_in = _mm_load_ps(&inputValues[16]);
+      matrix = _mm_load_ps(&fWeightMatrix0to1[o][16]);
+      c =  _mm_mul_ps(simd_in,matrix);
+      sum[oo] = _mm_add_ps(c,sum[oo]);
 
-    _mm_store_ss(&mWeights[o], sum);
+      simd_in = _mm_load_ps(&inputValues[20]);
+      matrix = (_mm_setr_ps(fWeightMatrix0to1[o][20],fWeightMatrix0to1[o][21],0.f,0.f));
+      c =  _mm_mul_ps(simd_in,matrix);
+      sum[oo] = _mm_add_ps(c,sum[oo]);
+
+    sum[oo] = _mm_hadd_ps(sum[oo],sum[oo]);
+    sum[oo] = _mm_hadd_ps(sum[oo],sum[oo]);
+
+    _mm_store_ss(&mWeights[o], sum[oo]);
+    }
   }
   
   {

commit e5ced3da7e600bc424edaf9c971fd9d193b122d1

fix index error, introduced in a8c98d35c23d18ccd803b3306cb4e023c2fd4020

MLP.C
@@ -827,32 +827,32 @@ inline float ReadMLP::GetMvaValue__( const std::vector<float>& inputValues )
     __m128 sum[4];
     for (int oo = 0 ; (oo<4&&o+oo<27-1) ; oo++) {
       __m128 simd_in = _mm_load_ps(&inputValues[0]);
-      __m128 matrix = _mm_load_ps(&fWeightMatrix0to1[o][0]);
+      __m128 matrix = _mm_load_ps(&fWeightMatrix0to1[o+oo][0]);
       sum[oo] = _mm_mul_ps(simd_in,matrix);
       simd_in = _mm_load_ps(&inputValues[4]);
-      matrix = _mm_load_ps(&fWeightMatrix0to1[o][4]);
+      matrix = _mm_load_ps(&fWeightMatrix0to1[o+oo][4]);
       __m128 c =  _mm_mul_ps(simd_in,matrix);
       sum[oo] = _mm_add_ps(c,sum[oo]);
 
       simd_in = _mm_load_ps(&inputValues[8]);
-      matrix = _mm_load_ps(&fWeightMatrix0to1[o][8]);
+      matrix = _mm_load_ps(&fWeightMatrix0to1[o+oo][8]);
       c =  _mm_mul_ps(simd_in,matrix);
       sum[oo] = _mm_add_ps(c,sum[oo]);
 
 
       simd_in = _mm_load_ps(&inputValues[12]);
-      matrix = _mm_load_ps(&fWeightMatrix0to1[o][12]);
+      matrix = _mm_load_ps(&fWeightMatrix0to1[o+oo][12]);
       c =  _mm_mul_ps(simd_in,matrix);
       sum[oo] = _mm_add_ps(c,sum[oo]);
 
 
       simd_in = _mm_load_ps(&inputValues[16]);
-      matrix = _mm_load_ps(&fWeightMatrix0to1[o][16]);
+      matrix = _mm_load_ps(&fWeightMatrix0to1[o+oo][16]);
       c =  _mm_mul_ps(simd_in,matrix);
       sum[oo] = _mm_add_ps(c,sum[oo]);
 
       simd_in = _mm_load_ps(&inputValues[20]);
-      matrix = (_mm_setr_ps(fWeightMatrix0to1[o][20],fWeightMatrix0to1[o][21],0.f,0.f));
+      matrix = (_mm_setr_ps(fWeightMatrix0to1[o+oo][20],fWeightMatrix0to1[o+oo][21],0.f,0.f));
       c =  _mm_mul_ps(simd_in,matrix);
       sum[oo] = _mm_add_ps(c,sum[oo]);
 